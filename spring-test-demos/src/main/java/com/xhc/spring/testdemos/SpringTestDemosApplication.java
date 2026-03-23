39bcf47effe5faf2263ec2665be4a60b86e71633
tonyxu <xuhuac@foxmail.com>
2026-03-23 15:57:38 +0800

    1.项目初始化

---
+++ spring-test-demos/src/main/java/com/xhc/spring/testdemos/SpringTestDemosApplication.java
@@ -0,0 +1,13 @@
+package com.xhc.spring.testdemos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestDemosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestDemosApplication.class, args);
+    }
+
+}
---
+++ spring-test-demos/src/test/java/com/xhc/spring/testdemos/SpringTestDemosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xhc.spring.testdemos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestDemosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
